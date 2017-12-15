791c4198b0165055a8148772cae7114424032a74
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2017-12-15 15:02:05 -0200

    Delete DNA.js

--- DNA.js
+++
@@ -1,9 +0,0 @@
-function DNA(){ //class
-	//Generates DNA, genes array(it's the force applied every frame)
-	//A gene per frame
-	this.genes = [];
-	for(var i = 0; i < frameLimit; i++){
-		this.genes[i] = p5.Vector.random2D();
-		this.genes[i].setMag(0.2);
-	}
-}
